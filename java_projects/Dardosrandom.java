f774803f60309b8aebe2da11504e489ae439c4f3
Pablo Corzo <pablo.corzocorella@eps.ceu.es>
2024-02-06 17:09:18 +0100

    añadido metodo recursivo

--- java_projects/Dardosrandom.java
+++ java_projects/Dardosrandom.java
@@ -7,16 +7,25 @@ public class Dardosrandom{
     // d/t = pi/4
     // can be simulated using only 1 4th of the circle
     // point is inside: x^2 + y^2 <= R 
+    static int i = 0;
+    static double val;
+    static Random r = new Random();
+    static int total = 10000;
+    static int counter = 0;
+    static int inside = 0;
 
-    public static double piAproximate(){
+    public static void setTest(int iterations){
+        counter = 0;
+        inside = 0;
+        total = iterations;
+    }
+
+    public static double piAproximateIterative(){
         double x;
         double y;
-        int total = 10000;
-        int inside = 0;
-        Random r = new Random();
         for(int i =0;i<total;i++){
-            x = r.nextDouble() * 0.99;
-            y = r.nextDouble() * 0.99;
+            x = r.nextDouble() * 1;
+            y = r.nextDouble() * 1;
             if(((Math.pow(x, 2))+(Math.pow(y, 2))) <= 1){
                 // System.out.println("Printilin celebratorio");
                 inside++;
@@ -28,7 +37,24 @@ public class Dardosrandom{
         return 4*((double)inside/(double)total);
     }
 
+    public static double piAproximateRecursive(int iterations){
+        if(i >= iterations){
+            return 4*((double)inside/(double)iterations);
+        } 
+        else{
+            i++;
+            System.out.println(i);
+            double x = r.nextDouble() * 1;
+            double y = r.nextDouble() * 1;
+            if(((Math.pow(x, 2))+(Math.pow(y, 2))) <= 1){
+                inside++;
+            }
+            return piAproximateRecursive(iterations);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(piAproximate());
+        // System.out.println(piAproximateIterative());
+        System.out.println(piAproximateRecursive(100));
     }
 }
