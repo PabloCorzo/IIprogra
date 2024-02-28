93d93de689d7c6e529a315be0b99ec6910d3bd22
Pablo Corzo <pablo.corzocorella@eps.ceu.es>
2024-02-28 16:57:27 +0100

    lambda

--- java_projects/LambdaEj2.java
+++ java_projects/LambdaEj2.java
@@ -5,14 +5,30 @@ import java.util.stream.*;
 public class LambdaEj2{
 
     //1
-
-    // static IntUnaryOperator sumOfN = x -> IntStream.rangeClosed(0,x).sum();
+    static int sumOfN(int x)
+    {
+        return IntStream.rangeClosed(0,x).sum();
+    }
 
     //2
-    // static IntUnaryOperator product = n -> IntStream.rangeClosed(1, n).reduce((n1,n2) -> n1*n2).orElse(1);
+    static int factorial(int n)
+    {
+        return IntStream.rangeClosed(1, n).reduce((n1,n2) -> n1*n2).orElse(1);
+    }
 
     //3
-    // static IntBinaryOperator power = (b,p) -> (int)Math.pow(b, p);
+    static int power(int base, int power)
+    {
+        IntStream is = IntStream.rangeClosed(1, power).map(e -> base);
+        int product = 1;
+        int[] arr = is.toArray();
+        for(int i: arr){
+            product *= i;
+        }
+        return product;
+    }
+
+
     
     //4
     static int sumOfList(List<Integer> list)
@@ -22,29 +38,97 @@ public class LambdaEj2{
 
 
     //5
-
+    static double avg(List<Integer> list)
+    {
+        return list.stream().mapToInt(Integer :: intValue).average().orElse(0);
+    }
+    static double avgDouble(List<Double> list)
+    {
+        return list.stream().mapToInt(Double :: intValue).average().orElse(0);
+    }
 
     //6
-
+    static double deviation(List<Integer> list)
+    {
+        double x = avg(list);
+        double average = avgDouble(list.stream().mapToDouble(Integer :: doubleValue).map(e -> Math.pow(e - x, 2)).boxed().collect(Collectors.toList()));
+        return Math.sqrt(average);
+        // return 0;
+    }
 
     //7
-
+    static int evenSum(int n)
+    {
+        int sum = IntStream
+        .rangeClosed(1, n)
+        .filter(e -> e%2 == 0)
+        .sum();
+        return sum;
+    }
 
     //8
+    static int evenSumofList(List<Integer> list)
+    {
+        int sum = list
+        .stream()
+        .filter(e -> e%2 == 0)
+        .mapToInt(Integer :: intValue)
+        .sum();
+        return sum;
+    }
+
     //9
+    static ArrayList<Integer> getEvenList(List<Integer> list)
+    {
+     ArrayList<Integer> arr = new ArrayList<>();
+     list.stream()
+     .filter(e -> e%2 == 0)
+     .forEach(e -> arr.add(e));
+     return arr;
+    }
+
     //10
+    static ArrayList<Integer> getEvens(int n)
+    {
+        ArrayList<Integer> arr = new ArrayList<>();
+        IntStream.rangeClosed(2, n)
+        .boxed()
+        .sorted(Comparator.reverseOrder())
+        .filter(e -> e % 2 == 0)
+        .forEach(e -> arr.add(e));
+        return arr;
+    }
+
     //11
+    static int listProduct(List<Integer> l1, List<Integer> l2)
+    {
+        if(l1.size() != l2.size()) return -1;
+        int sum = 0;
+        l1.stream().mapToInt(Integer :: intValue);
+        l2.stream().mapToInt(Integer :: intValue); 
+        return sum;
+    }
+
+
     //12
+
     //13
+
     //14
 
     static List<Integer> list2 = List.of(1,2,3,4,5, 6, 7, 8, 9,10);
 
     public static void main(String[] args) {
-        // System.out.println(sumOfN.applyAsInt(10));
-        // System.out.println(product.applyAsInt(6));
-        // System.out.println(product.applyAsInt(0));
-        // System.out.println(power.applyAsInt(2, 3));
-        System.out.println(sumOfList(list2));
+        System.out.println(sumOfN(10));
+        System.out.println(factorial(6));
+        System.out.println(power(2, 3));
+        System.out.println(power(5, 2));
+        System.out.println(power(2, 5));
+        System.out.println(avg(list2));
+        System.out.println(deviation(list2));
+        System.out.println(evenSum(10));
+        System.out.println(evenSumofList(list2));
+        System.out.println(getEvenList(list2));    
+        System.out.println(getEvens(16));
     }
 }
