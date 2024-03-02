fa6fa400cd5d09c95378b093cc48b68780d2ddee
Pablo Corzo <pablo.corzocorella@eps.ceu.es>
2024-03-02 19:33:55 +0100

    practica 2

--- java_projects/practica2/src/app/Main.java
+++ java_projects/practica2/src/app/Main.java
@@ -5,6 +5,6 @@ import maths.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("El numero Pi es " + Maths.piAproximateIterative(Integer.parseInt(args[0])));
+        System.out.println("El numero Pi es " + RecursiveDarts.piAproximateRecursive(100));
     }
 }
